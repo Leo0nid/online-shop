552aa0270a5fccb7021e65f36b38d139ef638a8f
Leo0nid <121210862+Leo0nid@users.noreply.github.com>
2023-08-18 17:03:23 +0600

    общий доступ к ключу firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
   
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
+  apiKey: 'AIzaSyB0Eb7QFYieGToqn4Aa4Gg1QaMxXv2QID0',
+  authDomain: 'online-shop-25171.firebaseapp.com',
+  projectId: 'online-shop-25171',
+  storageBucket: 'online-shop-25171.appspot.com',
+  messagingSenderId: '377005520774',
+  appId:'1:377005520774:web:2b409dc9110972f4141e07',
   
 };
 
@@ -20,6 +20,11 @@ const firebase = initializeApp(firebaseConfig);
 export const db = getFirestore(firebase);
 export const auth = getAuth(firebase)
 export const storage = getStorage(firebase)
+
+
+
+
+
 
 
 
