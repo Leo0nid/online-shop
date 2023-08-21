192d0369a50b3465f0ea2aadba82aab818ead708
Aelina <aelinakarymshakova@gmail.com>
2023-08-21 07:41:17 +0600

    Сделала динамические категории без данных в БД

--- src/components/pages/Categories.jsx
+++ src/components/pages/Categories.jsx
@@ -1,68 +1,39 @@
 import React, { Component } from 'react'
-import img1 from './../img/1.png'
-import img2 from './../img/2.png'
-import img3 from './../img/3.png'
-import img4 from './../img/4.png'
-import img5 from './../img/5.png'
+import axios from 'axios';
 
 
-export class Categories extends Component {
+export class Categories extends Component {  
 
   constructor(props) {
     super(props)
     this.state = {
-      Products: [
-        {
-          key: "T_shirts",
-          title: "Футболки",
-          img: "1.png",
-        },
-        {
-          key: "sweater",
-          title: "Кофты",
-          img: "2.png",
-        },
-        {
-          key: "Shirts",
-          title: "Рубашки",
-          img: "3.png",
-        },
-        {
-          key: "Sweatshirts",
-          title: "Свитшоты",
-          img: "4.png",
-        },
-        {
-          key: "Pants",
-          title: "Брюки",
-          img: "5.png",
-        },
-        {
-          key: "Bombers",
-          title: "Бомберы",
-          img: "6.png",
-        },
-
-      ]
+      products: []
     }
+  }
+
+  componentDidMount(){
+    axios.get('https://64e08b5750713530432c6be6.mockapi.io/products')
+      .then(response => {
+        this.setState({products: response.data})
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 
   render() {
     let prods = [];
-    let prodList = this.state.Products;
+    let prodList = this.state.products;
     for (let i = 0; i < prodList.length; i++) {
       console.log('scr/components/img/' + prodList[i].img);
       prods.push(
       <>
-        <img src={img1} />
-        <img src={img2} />
-        <img src={img3} />
-        <img src={img4} />
-        <img src={img5} />
-        <p className='itemTitle'>{prodList[i].title}</p>
+        <p className='itemTitle'>{prodList[i].name}</p>
+        <p>{prodList[i].price}</p>
       </>
       );
     }
+    
     return (
       <div>
         {prods}
